e792cb0c0fe10509fae1e0c5d9132af5371e6802
Conelius Kavoo <kavoocornelius7@gmail.com>
2026-08-27 12:36:41 +0300

    feat: add donor, organization, and admin routes with protected access

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import About from './pages/About'
 import HowItWorks from './pages/HowItWorks'
@@ -10,6 +10,22 @@ import Stories from './pages/Stories'
 import ApplyOrganization from './pages/ApplyOrganization'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import DonorDashboard from './pages/donor/Dashboard'
+import DonorProfile from './pages/donor/Profile'
+import DonorMyDonations from './pages/donor/MyDonations'
+import DonorRecurringDonations from './pages/donor/RecurringDonations'
+import OrganizationDashboard from './pages/organisation/Dashboard'
+import OrganizationProfile from './pages/organisation/Profile'
+import OrganizationDonors from './pages/organisation/Donors'
+import OrganizationDonations from './pages/organisation/Donations'
+import OrganizationBeneficiaries from './pages/organisation/Beneficiaries'
+import OrganizationInventory from './pages/organisation/Inventory'
+import OrganizationStories from './pages/organisation/Stories'
+import AdminDashboard from './pages/admin/Dashboard'
+import AdminApplications from './pages/admin/Applications'
+import AdminOrganisations from './pages/admin/Organisations'
+import AdminDonations from './pages/admin/Donations'
+import NotFound from './pages/NotFound'
 
 
 function App() {
@@ -17,6 +33,7 @@ function App() {
 		<>
 		<Navbar />
 		<Routes>
+			{/* Public Routes */}
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/how-it-works" element={<HowItWorks />} />
@@ -26,6 +43,135 @@ function App() {
 			<Route path="/apply-organization" element={<ApplyOrganization />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
+
+			{/* Donor Routes */}
+			<Route
+				path="/donor/dashboard"
+				element={
+					<ProtectedRoute requiredRole="donor">
+						<DonorDashboard />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/donor/profile"
+				element={
+					<ProtectedRoute requiredRole="donor">
+						<DonorProfile />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/donor/donations"
+				element={
+					<ProtectedRoute requiredRole="donor">
+						<DonorMyDonations />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/donor/recurring-donations"
+				element={
+					<ProtectedRoute requiredRole="donor">
+						<DonorRecurringDonations />
+					</ProtectedRoute>
+				}
+			/>
+
+			{/* Organization Routes */}
+			<Route
+				path="/organization/dashboard"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationDashboard />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/profile"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationProfile />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/donors"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationDonors />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/donations"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationDonations />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/beneficiaries"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationBeneficiaries />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/inventory"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationInventory />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/organization/stories"
+				element={
+					<ProtectedRoute requiredRole="organization">
+						<OrganizationStories />
+					</ProtectedRoute>
+				}
+			/>
+
+			{/* Admin Routes */}
+			<Route
+				path="/admin/dashboard"
+				element={
+					<ProtectedRoute requiredRole="admin">
+						<AdminDashboard />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/admin/applications"
+				element={
+					<ProtectedRoute requiredRole="admin">
+						<AdminApplications />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/admin/organizations"
+				element={
+					<ProtectedRoute requiredRole="admin">
+						<AdminOrganisations />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/admin/donations"
+				element={
+					<ProtectedRoute requiredRole="admin">
+						<AdminDonations />
+					</ProtectedRoute>
+				}
+			/>
+
+			{/* Catch-all Route */}
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 		</>
 	)
